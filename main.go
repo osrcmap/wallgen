cb4f8689549003b9382bd78bed0dbe838a5dae60
osrcmap <mapeoscar123@gmail.com>
2026-05-11 18:09:26 -0500

    ci: add goreleaser for cross-platform releases

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 	"github.com/osrcmap/wallgen/cmd"
 )
 
+// Set by GoReleaser via -ldflags -X at build time.
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	if err := cmd.Root().Execute(); err != nil {
+	root := cmd.Root()
+	root.Version = fmt.Sprintf("%s (commit %s, built %s)", version, commit, date)
+	if err := root.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
